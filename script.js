db53c7e77678424f8654e651085e641e4ac1f74a
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-08-30 21:59:47 -0300

    Subo semana 5 y 6

--- script.js
+++ script.js
@@ -95,8 +95,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const dataLine1 = {
     labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'],
     datasets: [{
-      label: 'Km recorridos en Semana 3',
-      data: [10, 16, 10, 14, 17],
+      label: 'Km recorridos en Semana 5',
+      data: [21, 20, 13, 18, 29],
       borderColor: 'rgba(75, 192, 192, 1)',
       fill: true,
       tension: 0.1,
@@ -107,8 +107,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const dataLine2 = {
     labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5'],
     datasets: [{
-      label: 'Km recorridos en Semana 4',
-      data: [9, 10, 10, 14, 20],
+      label: 'Km recorridos en Semana 6',
+      data: [11, 16, 11, 14, 15],
       borderColor: 'rgba(153, 102, 255, 1)',
       fill: true,
       tension: 0.1
